65ecb168f5b91933164ee01d28ad725190139572
Tammie Chung <tammie.chung11@gmail.com>
2019-09-25 21:33:13 +0000

    testing using node.js sample server/client code

--- client.js
+++ client.js
@@ -1,25 +1,42 @@
-const net = require('net');
-const { setupInput } = require('./input');
+// const net = require('net');
+// const { setupInput } = require('./input');
 
-//const connect = function() {
-const conn = net.createConnection({
-  host: '172.46.0.146', // change to IP address if connecting to another computer
-  port: 3000
+// //const connect = function() {
+// const conn = net.createConnection({
+//   host: '172.46.0.146', // change to IP address if connecting to another computer
+//   port: 3000
+// });
+
+// conn.setEncoding('utf8'); // interpret data as text
+
+// conn.on('data', (data) => {
+//   console.log('Server says: ', data);
+// });
+
+// conn.on('connection', (server) => {
+//   server.write('Hello from client!');
+//   server.setEncoding('utf8');
+// });
+// //return conn;
+// //}
+// //connect();
+// //setupInput(connect);
+
+// //module.exports = { connect };
+
+var net = require('net');
+
+var client = new net.Socket();
+client.connect(3000, '172.46.0.146', function() {
+	console.log('Connected');
+	client.write('Hello, server! Love, Client.');
 });
 
-conn.setEncoding('utf8'); // interpret data as text
-
-conn.on('data', (data) => {
-  console.log('Server says: ', data);
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
 });
 
-conn.on('connection', (server) => {
-  server.write('Hello from client!');
-  server.setEncoding('utf8');
+client.on('close', function() {
+	console.log('Connection closed');
 });
-//return conn;
-//}
-//connect();
-//setupInput(connect);
-
-//module.exports = { connect };
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const net = require('net');
+// const net = require('net');
 
-const server = net.createServer();
+// const server = net.createServer();
 
-server.on('connection', (client) => {
-  console.log('New client connected!');
-  client.write('What file would you like?');
-  client.setEncoding('utf8'); // interpret data as text
+// server.on('connection', (client) => {
+//   console.log('New client connected!');
+//   client.write('What file would you like?');
+//   client.setEncoding('utf8'); // interpret data as text
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server listening on port 3000!');
+// });
+
+// server.on('data', (client) => {
+//   client.on('data', (data) => {
+//     console.log('Message from client: ', data)
+//   });
+// })
+
+var net = require('net');
+
+var server = net.createServer(function(socket) {
+	socket.write('Hello client!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000!');
-});
-
-server.on('data', (client) => {
-  client.on('data', (data) => {
-    console.log('Message from client: ', data)
-  });
-})
+server.listen(3000, '172.46.0.146');
