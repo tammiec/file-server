eb1d03ab1ae9eaf8dad3096d399149d5b52f73fb
Tammie Chung <tammie.chung11@gmail.com>
2019-09-25 20:28:39 +0000

    testing server/client response

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const server = net.createServer();
 
 server.on('connection', (client) => {
   console.log('New client connected!');
-  client.write('Hello there!');
+  client.write('What file would you like?');
   client.setEncoding('utf8'); // interpret data as text
-  client.on('data', (data) => {
-    console.log('Message from client: ', data)
-  });
 });
 
 server.listen(3000, () => {
   console.log('Server listening on port 3000!');
 });
+
+server.on('data', (client) => {
+  client.on('data', (data) => {
+    console.log('Message from client: ', data)
+  });
+})
