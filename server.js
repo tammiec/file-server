ee69b520d31dd50e16b375aacb8846eb16139def
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-25 21:56:26 +0000

    Able to send text through the server

--- server.js
+++ server.js
@@ -10,18 +10,45 @@
 
 // server.listen(3000, () => {
 //   console.log('Server listening on port 3000!');
-// });
-
-// server.on('data', (client) => {
-//   client.on('data', (data) => {
-//     console.log('Message from client: ', data)
-//   });
-// })
 
 var net = require('net');
+let fs = require(`fs`);
 
-var server = net.createServer(function(socket) {
-	socket.write('Hello client!\n');
-});
 
-server.listen(3000, '172.46.0.146');
+net.createServer(function(socket) {
+  socket.write('Hello client!\n');
+  let fileName;
+  // });
+
+  socket.on('data', (data) => {
+    fileName = String(data).trim();
+    if (!fileName.includes(".txt")) {
+      fileName += ".txt"
+    }
+    console.log(`Looking for file ${fileName}`);
+
+    //Find the file within the /data-files subdirectory
+
+    const findFile = function(file, callback) {
+      fs.readFile(`./data-files/${file}`, 'utf8', (error, data) => {
+        if (!error) {
+          callback(data);
+        } else {
+          console.log("The file does not exist!");
+          callback(undefined);
+        }
+
+      });
+      //Send it back
+    }
+
+    findFile(fileName, (data) => {
+      socket.write(data);
+    });
+
+
+
+  });
+
+}).listen(3000);
+
