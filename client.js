d12e64866a055366ffa8c1e1a70c28b42f69f221
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-27 18:05:18 +0000

    got the client.js to now work with server.js

--- client.js
+++ client.js
@@ -1,42 +1,61 @@
-// const net = require('net');
-// const { setupInput } = require('./input');
+var net = require('net');
+let readline = require(`readline`);
 
-// //const connect = function() {
-// const conn = net.createConnection({
-//   host: '172.46.0.146', // change to IP address if connecting to another computer
-//   port: 3000
-// });
+const connect = function() {
+  const conn = net.createConnection(
+    {
+      host: 'localhost', // change to IP address if connecting to another computer
+      port: 3000
+    });
 
-// conn.setEncoding('utf8'); // interpret data as text
+  conn.setEncoding('utf8'); // interpret data as text
 
-// conn.on('data', (data) => {
-//   console.log('Server says: ', data);
-// });
+  conn.on('connect', () => {
+    conn.write('Hello from client!');
+    // conn.setEncoding('utf8');
+  });
 
-// conn.on('connection', (server) => {
-//   server.write('Hello from client!');
-//   server.setEncoding('utf8');
-// });
-// //return conn;
-// //}
-// //connect();
-// //setupInput(connect);
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  }); //receives data from server?
+
+  return conn;
+}
+
+
+const setupInput = function(conn) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on('line', (data) => {
+    conn.write(data);
+  });
+
+
+  // return stdin;
+}
+
+setupInput(connect()); //Needs return value of connect
 
 // //module.exports = { connect };
 
-var net = require('net');
 
+/*
 var client = new net.Socket();
 client.connect(3000, '172.46.0.146', function() {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+  console.log('Connected');
+  client.write('Hello, server! Love, Client.');
 });
 
 client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
+  console.log('Received: ' + data);
+  client.destroy(); // kill client after server's response
 });
 
 client.on('close', function() {
-	console.log('Connection closed');
+  console.log('Connection closed');
 });
+
+*/
